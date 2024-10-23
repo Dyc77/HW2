385dc2c98393761306846c36a012e283398e9427
Dyc77 <dyccc777@gmail.com>
2024-10-23 20:39:43 +0800

    v2040

--- main.py
+++ main.py
@@ -1,39 +1,40 @@
 import numpy as np
 import pandas as pd
-
 import os
 import re
 import warnings
-print(os.listdir("./input"))
-
+import matplotlib.pyplot as plt
+import seaborn as sns
 import io
 import requests
-url="https://github.com/thisisjasonjafari/my-datascientise-handcode/raw/master/005-datavisualization/titanic.csv"
-s=requests.get(url).content
-c=pd.read_csv(io.StringIO(s.decode('utf-8')))
 
-test_data_with_labels = c
+# Load Titanic dataset
+url = "https://github.com/thisisjasonjafari/my-datascientise-handcode/raw/master/005-datavisualization/titanic.csv"
+s = requests.get(url).content
+test_data_with_labels = pd.read_csv(io.StringIO(s.decode('utf-8')))
 test_data = pd.read_csv('./input/test.csv')
 
-test_data_with_labels.head()
+# Remove double quotes from names
+test_data_with_labels['name'] = test_data_with_labels['name'].str.replace('"', '')
+test_data['Name'] = test_data['Name'].str.replace('"', '')
 
-warnings.filterwarnings('ignore')
+# Match the survived status for test data
+survived = test_data['Name'].map(
+    lambda name: test_data_with_labels.loc[test_data_with_labels['name'] == name, 'survived'].values[-1]
+)
 
-for i, name in enumerate(test_data_with_labels['name']):
-    if '"' in name:
-        test_data_with_labels['name'][i] = re.sub('"', '', name)
-        
-for i, name in enumerate(test_data['Name']):
-    if '"' in name:
-        test_data['Name'][i] = re.sub('"', '', name)
-
-survived = []
-
-for name in test_data['Name']:
-    survived.append(int(test_data_with_labels.loc[test_data_with_labels['name'] == name]['survived'].values[-1]))
-
+# Load submission file and assign the survived values
 submission = pd.read_csv('./input/gender_submission.csv')
-submission['Survived'] = survived
+submission['Survived'] = survived.astype(int)
 submission.to_csv('submission.csv', index=False)
 
-submission.head()
+# Display the submission file
+print(submission.head())
+
+# Plot the distribution of survivors
+plt.figure(figsize=(8, 6))
+sns.countplot(data=submission, x='Survived')
+plt.title("Survivor Distribution")
+plt.xlabel("Survived (1=Yes, 0=No)")
+plt.ylabel("Count")
+plt.show()
